1bf1a4e1765ef77abb12a7133cfc59367e8c7ccd
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-11-23 14:21:52 +0900

    FEAT / create API Common Response

---
+++ src/main/java/umc/spring/UmcMission8Application.java
@@ -0,0 +1,13 @@
+package umc.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcMission8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmcMission8Application.class, args);
+    }
+
+}
---
+++ src/test/java/umc/spring/UmcMission8ApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcMission8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
