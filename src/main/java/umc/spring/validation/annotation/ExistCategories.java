43a15019db8a62c151944868fe3abf23de13f10f
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-12-14 17:50:42 +0900

    #1 FEAT / Annotation Validation

---
+++ src/main/java/umc/spring/validation/annotation/ExistCategories.java
@@ -0,0 +1,21 @@
+package umc.spring.validation.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import umc.spring.validation.validator.CategoriesExistValidator;
+
+@Documented
+@Constraint(validatedBy = CategoriesExistValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExistCategories {
+
+    String message() default "해당하는 카테고리가 존재하지 않습니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
--- src/main/java/umc/spring/web/dto/MemberRequestDTO.java
+++ src/main/java/umc/spring/web/dto/MemberRequestDTO.java
@@ -1,18 +1,31 @@
 package umc.spring.web.dto;
 
 import java.util.List;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Getter;
+import umc.spring.validation.annotation.ExistCategories;
 
 public class MemberRequestDTO {
+
     @Getter
-    public static class JoinDTO{
+    public static class JoinDto{
+        @NotBlank
         String name;
+        @NotNull
         Integer gender;
+        @NotNull
         Integer birthYear;
+        @NotNull
         Integer birthMonth;
+        @NotNull
         Integer birthDay;
+        @Size(min = 5, max = 12)
         String address;
+        @Size(min = 5, max = 12)
         String specAddress;
+        @ExistCategories
         List<Long> preferCategory;
     }
 }
